c2f01fd61a7f1d68c37cf3148a2f80caaaf5f94a
anandwana001 <14etcit001@technonjr.org>
2017-10-25 23:52:58 +0530

    initial commit

---
+++ anagrams_starter/app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -0,0 +1,168 @@
+package com.google.engedu.anagrams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Random;
+
+public class AnagramDictionary {
+
+    private static final int MIN_NUM_ANAGRAMS = 5;
+    private static final int DEFAULT_WORD_LENGTH = 3;
+    private static final int MAX_WORD_LENGTH = 7;
+    private Random random = new Random();
+
+    private int wordLength = DEFAULT_WORD_LENGTH;
+
+
+    public ArrayList<String> wordList = new ArrayList<>();
+    public HashMap<String, ArrayList<String>> lettersToWord = new HashMap<>();
+    public HashSet<String> wordSet = new HashSet<>();
+
+    public HashMap<Integer, ArrayList<String>> sizeToWords = new HashMap<>();
+
+    public AnagramDictionary(InputStream wordListStream) throws IOException {
+
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String word = line.trim();
+
+            //add to the arrayList data structure
+            wordList.add(word);
+
+            //add the same word to a hashSet and hashMap
+            wordSet.add(word);
+
+            String sortedWord = sortLetters(word);
+
+            //if hm already contains the key, we add it to the same key
+            if (lettersToWord.containsKey(sortedWord)) {
+                lettersToWord.get(sortedWord).add(word);
+            }
+            //else we create a new arrayList and add that as the value to that key in the hm
+            else {
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(word);
+                lettersToWord.put(sortedWord, temp);
+            }
+
+            //check word length and store in sizeToWords
+            if (sizeToWords.containsKey(word.length())) {
+                sizeToWords.get(word.length()).add(word);
+            } else {
+                ArrayList<String> temp = new ArrayList<>();
+                temp.add(word);
+                sizeToWords.put(word.length(), temp);
+            }
+
+        }
+    }
+
+    public boolean isGoodWord(String word, String base) {
+
+        //if valid, then hashSet has that word in it && the word should not be a subString of the baseWord; i.e prefix or postfix
+        return wordSet.contains(word) && !word.contains(base);
+    }
+
+    public ArrayList<String> getAnagrams(String targetWord) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        //sort the target word
+        String sortedTargetWord = sortLetters(targetWord);
+
+        //first step is to iterate through all 10000 words and find the anagrams
+        for (String word : wordList) {
+            //sort the word
+            String sortedWord = sortLetters(word);
+
+            //if it matches to sortedTargetWord, then it's an anagram of it
+            if (sortedTargetWord.equals(sortedWord)) {
+                //add the original word
+                result.add(word);
+            }
+        }
+
+        return result;
+    }
+
+    public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> result = new ArrayList<String>();
+
+        String alphabets = "abcdefghijklmnopqrstuvwxyz";
+        for (int i = 0; i < alphabets.length(); i++) {
+
+            String tempString = word;
+
+            //add every letter and get the key
+            tempString += alphabets.charAt(i);
+
+            String key = sortLetters(tempString);
+
+            //check if that key exists
+            if (lettersToWord.containsKey(key)) {
+
+                //get all the values for that key
+                ArrayList<String> tempList = lettersToWord.get(key);
+
+                //check if the obtained words are notGoodWords again
+                ArrayList<String> removeList = new ArrayList<>();
+                for (String test : tempList) {
+                    if (!isGoodWord(test, word)) {
+                        removeList.add(test);
+                    }
+                }
+
+                //remove all the notGoodWords
+                result.removeAll(removeList);
+
+                //add the list to the remaining list to be returned
+                result.addAll(tempList);
+            }
+        }
+
+        return result;
+    }
+
+    public String pickGoodStarterWord() {
+
+        while (true) {
+
+            //get all words with 3/4/5 letters and pick from them only
+            ArrayList<String> tempList = sizeToWords.get(wordLength);
+
+            //generate a random number between 0 and sizeOf list obtained
+            Random random = new Random();
+            int num = random.nextInt(tempList.size());
+
+
+            //pick random word from the arrayList
+            //String randomWord = wordList.get(num);
+            String randomWord = tempList.get(num);
+
+            //get all the anagrams for that random word
+            ArrayList<String> arrayList = getAnagramsWithOneMoreLetter(randomWord);
+
+            //validate the conditions given
+            if ((randomWord.length() == wordLength) && arrayList.size() > MIN_NUM_ANAGRAMS) {
+
+                //increment the wordLength for next stage
+                if (wordLength < MAX_WORD_LENGTH) wordLength++;
+                return randomWord;
+            }
+        }
+
+    }
+
+    public String sortLetters(String word) {
+        char[] words = word.toCharArray();
+        Arrays.sort(words);
+        return new String(words);
+    }
+}
---
+++ anagrams_starter/app/src/main/java/com/google/engedu/anagrams/AnagramsActivity.java
@@ -0,0 +1,135 @@
+package com.google.engedu.anagrams;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+
+public class AnagramsActivity extends AppCompatActivity {
+
+    public static final String START_MESSAGE = "Find as many words as possible that can be formed by adding one letter to <big>%s</big> (but that do not contain the substring %s).";
+    private AnagramDictionary dictionary;
+    private String currentWord;
+    private ArrayList<String> anagrams;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anagrams);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        AssetManager assetManager = getAssets();
+        try {
+            InputStream inputStream = assetManager.open("words.txt");
+            dictionary = new AnagramDictionary(inputStream);
+        } catch (IOException e) {
+            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        // Set up the EditText box to process the content of the box when the user hits 'enter'
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setImeOptions(EditorInfo.IME_ACTION_GO);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    processWord(editText);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+
+    private void processWord(EditText editText) {
+        TextView resultView = (TextView) findViewById(R.id.resultView);
+        String word = editText.getText().toString().trim().toLowerCase();
+        if (word.length() == 0) {
+            return;
+        }
+        String color = "#cc0029";
+        if (dictionary.isGoodWord(word, currentWord) && anagrams.contains(word)) {
+            anagrams.remove(word);
+            color = "#00aa29";
+        } else {
+            word = "X " + word;
+        }
+        resultView.append(Html.fromHtml(String.format("<font color=%s>%s</font><BR>", color, word)));
+        editText.setText("");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_anagrams, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean defaultAction(View view) {
+        TextView gameStatus = (TextView) findViewById(R.id.gameStatusView);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        TextView resultView = (TextView) findViewById(R.id.resultView);
+        if (currentWord == null) {
+            currentWord = dictionary.pickGoodStarterWord();
+            anagrams = dictionary.getAnagramsWithOneMoreLetter(currentWord);
+            gameStatus.setText(Html.fromHtml(String.format(START_MESSAGE, currentWord.toUpperCase(), currentWord)));
+            fab.setImageResource(android.R.drawable.ic_menu_help);
+            fab.hide();
+            resultView.setText("");
+            editText.setText("");
+            editText.setEnabled(true);
+            editText.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+        } else {
+            editText.setText(currentWord);
+            editText.setEnabled(false);
+            fab.setImageResource(android.R.drawable.ic_media_play);
+            currentWord = null;
+            resultView.append(TextUtils.join("\n", anagrams));
+            gameStatus.append(" Hit 'Play' to start again");
+        }
+        return true;
+    }
+}
